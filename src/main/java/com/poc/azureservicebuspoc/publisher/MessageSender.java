fbeb9e3ef053d24eb4d6db23fd19d9544ed3fb27
aashishthakranposten <106076032+aashishthakranposten@users.noreply.github.com>
2022-06-23 04:06:40 +0200

    Update MessageSender.java

--- src/main/java/com/poc/azureservicebuspoc/publisher/MessageSender.java
+++ src/main/java/com/poc/azureservicebuspoc/publisher/MessageSender.java
@@ -1,54 +1,66 @@
 package com.poc.azureservicebuspoc.publisher;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.annotation.PostConstruct;
+import javax.jms.ObjectMessage;
 
 import com.poc.azureservicebuspoc.model.Consignment;
 import com.poc.azureservicebuspoc.model.ConsignmentData;
-import com.poc.azureservicebuspoc.model.ConsignmentItem;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
 public class MessageSender {
 
     //Queue/Topic name
-    private static final String DESTINATION_NAME = "test-topic";
+    private static final String DESTINATION_NAME = "test-topic6";
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    @PostConstruct
+    //private static AtomicLong counter = new AtomicLong(0);
+
+    //@PostConstruct
     public void postConstruct() {
         postMessage();
     }
 
     public void postMessage() {
-        log.info("Sending message");
-        jmsTemplate.convertAndSend(DESTINATION_NAME, createConsignmentData());
+
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void sendMessage() {
+        int count = 0;
+        try {
+            for(int i = 0; i < 100; i++) {
+                jmsTemplate.convertAndSend(DESTINATION_NAME, createConsignmentData());
+                count++;
+            }
+            System.exit(0);
+            //counter.getAndIncrement();
+
+        } catch (Exception ex) {
+
+        }
+        System.out.println("=========================== Finished sending messages Total messages sent in 1 run => " + count);
     }
 
     private ConsignmentData createConsignmentData() {
-        return ConsignmentData.builder()
-            .batchId(UUID.randomUUID())
-            .consignments(List.of(createConsignment()))
-            .build();
+        ConsignmentData consignmentData = new ConsignmentData();
+        consignmentData.setBatchId(UUID.randomUUID());
+        consignmentData.setConsignments(Arrays.asList(createConsignment()));
+        return consignmentData;
     }
 
     private Consignment createConsignment() {
-        UUID consignmentId = UUID.randomUUID();
-        ConsignmentItem item = ConsignmentItem.builder()
-            .id(UUID.randomUUID())
-            .consignmentId(consignmentId)
-            .build();
-        return Consignment.builder()
-            .uuid(consignmentId)
-            .items(List.of(item))
-            .build();
+        Consignment consignment = new Consignment();
+        consignment.setUuid(UUID.randomUUID());
+        return consignment;
     }
 }
